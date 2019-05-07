3f5f6c8ffd86e439a79ef4501a52e20bf0e6e59b
suijingfeng <18949883232@163.com>
2019-05-08 04:48:41 +0800

    improve

--- code/renderer_vulkan/vk_khr_display.c
+++ code/renderer_vulkan/vk_khr_display.c
@@ -3,6 +3,32 @@
 
 VkDisplayKHR DisplayArray[4];
 
+void vk_get_DisplayModeProps(VkDisplayKHR hDis)
+{
+    uint32_t i = 0; 
+    uint32_t disModeCnt = 0;
+    
+    VK_CHECK( qvkGetDisplayModePropertiesKHR(vk.physical_device, hDis, &disModeCnt, NULL) );
+
+    if (disModeCnt > 0)
+    {
+        VkDisplayModePropertiesKHR * pModeProps = (VkDisplayModePropertiesKHR *) 
+            malloc( disModeCnt * sizeof(VkDisplayModePropertiesKHR) );
+
+        memset(pModeProps, 0, disModeCnt * sizeof(VkDisplayModePropertiesKHR));
+        VK_CHECK( qvkGetDisplayModePropertiesKHR(vk.physical_device, hDis, &disModeCnt, pModeProps) );
+
+        for (i = 0; i<disModeCnt; ++i)
+        {
+            ri.Printf(PRINT_ALL, " Display Mode Propertie %d: %dx%d %.1f hz. \n",
+                i, pModeProps[i].parameters.visibleRegion.width, pModeProps[i].parameters.visibleRegion.height,
+                pModeProps[i].parameters.refreshRate / 1000.0f  );
+        }
+
+        free(pModeProps);
+    }
+}
+
 
 void vk_displayInfo_f(void)
 {
@@ -10,12 +36,16 @@ void vk_displayInfo_f(void)
     uint32_t i = 0;
     //VkDisplayPropertiesKHR* pDisplayProps = NULL;
     // I have no idea, this just not work on ubuntu16.04 384.130 nvidia driver
+    // ubuntu18.04 Gnome, Nvidia 390.116, Aspire-V3-772, GTX760 not working
     VK_CHECK( qvkGetPhysicalDeviceDisplayPropertiesKHR(vk.physical_device, &displayCnt, NULL) );
 
     if (displayCnt != 0)
     {
         VkDisplayPropertiesKHR* pDisplayProps = 
             (VkDisplayPropertiesKHR* )malloc( displayCnt * sizeof(VkDisplayPropertiesKHR) );
+        
+        memset(pDisplayProps, 0, displayCnt * sizeof(VkDisplayPropertiesKHR));
+
         VK_CHECK( qvkGetPhysicalDeviceDisplayPropertiesKHR(vk.physical_device, &displayCnt, pDisplayProps) );
         
         ri.Printf(PRINT_ALL, " -------- Total %d Minitor find --------\n", displayCnt);
@@ -41,29 +71,10 @@ void vk_displayInfo_f(void)
             DisplayArray[i] = pDisplayProps[i].display;
         
         free(pDisplayProps);
+
+        // ensure that DisplayArray[0] != VK_NULL_HANDLE
+        //
+        vk_get_DisplayModeProps(DisplayArray[0]);
     }
-
-
-    uint32_t disModeCnt = 0;
-    
-    VK_CHECK( qvkGetDisplayModePropertiesKHR(vk.physical_device, DisplayArray[0], &disModeCnt, NULL) );
-
-    if (disModeCnt > 0)
-    {
-        VkDisplayModePropertiesKHR * pModeProps = (VkDisplayModePropertiesKHR *) 
-            malloc( disModeCnt * sizeof(VkDisplayModePropertiesKHR) );
-
-        VK_CHECK( qvkGetDisplayModePropertiesKHR(vk.physical_device, DisplayArray[0], &disModeCnt, pModeProps) );
-
-        for (i = 0; i<disModeCnt; ++i)
-        {
-            ri.Printf(PRINT_ALL, " Display Mode Propertie %d: %dx%d %.1f hz. \n",
-                i, pModeProps[i].parameters.visibleRegion.width, pModeProps[i].parameters.visibleRegion.height,
-                pModeProps[i].parameters.refreshRate / 1000.0f  );
-        }
-
-        free(pModeProps);
-    }
-
 
 }
