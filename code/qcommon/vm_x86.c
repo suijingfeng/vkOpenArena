3a7536893bb2bda0fe9ba3378a62c24c13c40e1a
suijingfeng <18949883232@189.com>
2019-09-01 11:58:52 +0800

    want commit, dont know how can I link the qvmcall64 with vs2017

--- code/qcommon/vm_x86.c
+++ code/qcommon/vm_x86.c
@@ -1768,11 +1768,6 @@ VM_CallCompiled
 This function is called directly by the generated code
 ==============
 */
-
-#if defined(_MSC_VER) && defined(idx64)
-extern uint8_t qvmcall64(int *programStack, int *opStack, intptr_t *instructionPointers, unsigned char *dataBase);
-#endif
-
 intptr_t VM_CallCompiled(vm_t *vm, int *args)
 {
 	unsigned char stack[OPSTACK_SIZE + 15];
@@ -1807,7 +1802,10 @@ intptr_t VM_CallCompiled(vm_t *vm, int *args)
 
 #ifdef _MSC_VER
   #if idx64
-	opStackOfs = qvmcall64(&programStack, opStack, vm->instructionPointers, vm->dataBase);
+	extern uint8_t qvmcall64(int *programStack, int *opStack, intptr_t *instructionPointers, unsigned char *dataBase);
+	typedef uint8_t (* qvmcall64_t)(int *programStack, int *opStack, intptr_t *instructionPointers, unsigned char *dataBase);
+	qvmcall64_t fnQvmcall64 = qvmcall64;
+	opStackOfs = fnQvmcall64(&programStack, opStack, vm->instructionPointers, vm->dataBase);
   #else
 	__asm
 	{
