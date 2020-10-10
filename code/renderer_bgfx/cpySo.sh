dd026f28dd9d3f3d8111711819553166b2452962
suijingfeng <suijingfeng@loongson.cn>
2020-10-10 22:50:52 +0800

    remove renderer_bgfx

--- code/renderer_bgfx/cpySo.sh
+++
@@ -1,1 +0,0 @@
-cp ./bin_x64/renderer_bgfx_x86_64.so ../../../build/release-linux-x86_64/
--- code/renderer_bgfx/shaders/PortalClip.sh
+++
@@ -1,7 +0,0 @@
-uniform vec4 u_PortalClipEnabled;
-uniform vec4 u_PortalPlane;
-
-bool PortalClipped(vec3 position)
-{
-	return int(u_PortalClipEnabled.x) != 0 && dot(position, u_PortalPlane.xyz) - u_PortalPlane.w < 0.0;
-}
